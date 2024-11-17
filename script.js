15246c6a5169e859ac70b0f769e29609372a335b
pierreaxe <pierre@robotize.com>
2024-11-17 13:37:35 +0100

    Updates

--- script.js
+++ script.js
@@ -308,8 +308,8 @@ $(document).ready(async function() {
             console.log('Search button is pressed!');
             // Get the location coordinates
             const locationCoordinates = await getLocationCoordinates(location);
-            print(locationCoordinates);
-            print(locationCoordinates.lat);
+            console.log(locationCoordinates);
+            console.log(locationCoordinates.lat);
     
             // Fetch hotels by coordinates
             const hotelsData = await fetchHotelsByCoordinates(locationCoordinates.lat, locationCoordinates.lng);
