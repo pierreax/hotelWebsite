3d227058c191b59684a3a40f774dce0781ffd7d9
pierreaxe <pierre@robotize.com>
2024-11-22 09:55:41 +0100

    Updates

--- script.js
+++ script.js
@@ -602,7 +602,7 @@ $(document).ready(function() {
             }
             const { lat, lng } = coords;
             console.log(lat, lng);
-            const getHotelsByCoordinatesUrlWithParams = `${getHotelsByCoordinatesUrl}?latitude=${lat}&longitude=${lng}&radius=10&radiusUnit=KM&hotelSource=ALL`;
+            const getHotelsByCoordinatesUrlWithParams = `${getHotelsByCoordinatesUrl}?lat=${lat}&lng=${lng}&radius=10&radiusUnit=KM&hotelSource=ALL`;
         
             const hotelsResponse = await fetch(getHotelsByCoordinatesUrlWithParams, {
                 headers: {
